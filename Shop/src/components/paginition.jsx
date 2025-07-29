41a606f3be231257c7b256738a934894b85d74ab
sananasgarov <prog58816@gmail.com>
2025-07-29 18:40:50 +0400

    A

--- Shop/src/components/paginition.jsx
+++ Shop/src/components/paginition.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-
 function Paginition() {
   return (
     <div className="flex justify-center mt-6 space-x-2">
-      {[1, 2, 3, 4, 5].map((page,index) => (
-        <button
-          key={index}
-          className={`px-4 py-2 border rounded-lg text-sm font-medium transition duration-200`}
-        >{page}
-        </button>
-      ))}
+      <button
+        className={`px-4 py-2 border rounded-lg text-sm font-medium transition duration-200`}
+      ></button>
     </div>
   );
 }
