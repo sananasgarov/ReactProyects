b59d046b991bb04f77aad48033414d86973c01c6
sananasgarov <prog58816@gmail.com>
2025-08-15 13:47:03 +0400

    A

--- MainProyect/src/pages/home/index.jsx
+++ MainProyect/src/pages/home/index.jsx
@@ -1,10 +1,10 @@
 import SliderSec from "../../components/HomeSec/slidersec";
-import HomeAbout from "../../components/HomeSec/HomeAbout";
 import Cars from "../../components/HomeSec/cars";
 import Advandec from "../../components/HomeSec/advancedsec";
 import Partnyor from "../../components/HomeSec/partnyorsec";
 import QuestionSec from "../../components/HomeSec/questionSec";
 import Delivery from "../../components/HomeSec/deliverySec";
+import HomeAbout from "../../components/HomeSec/homeabout";
 function Home() {
   return (
     <div className="mx-auto">
